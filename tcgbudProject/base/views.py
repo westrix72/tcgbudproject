39c4b896b181f3346c236a4609c818dc7cc2cb45
westrix72 <lfeliperebello@live.com>
2023-10-29 18:54:57 -0300

    Moving templates

--- tcgbudProject/base/views.py
+++ tcgbudProject/base/views.py
@@ -10,7 +10,12 @@ rooms = [
 # Create your views here.
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
