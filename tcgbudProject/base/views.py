3eb7a0e83411d4a8b1b29f198ce6e2f809701b75
westrix72 <lfeliperebello@live.com>
2023-10-29 16:53:49 -0300

    Implementing django tags

--- tcgbudProject/base/views.py
+++ tcgbudProject/base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
-def profile(request):
-    return render(request, 'profile.html')
+def room(request):
+    return render(request, 'room.html')
