a0d14ae5c562dce9aafa8a53fbc94d8ecf2361a2
westrix72 <lfeliperebello@live.com>
2023-10-30 22:01:08 -0300

    using rooms from database

--- tcgbudProject/base/views.py
+++ tcgbudProject/base/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-rooms = [
-    {'id': 1, 'name': 'Room 1'},
-    {'id': 2, 'name': 'Room 2'},
-    {'id': 3, 'name': 'Room 3'},
-]
+from .models import Room
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
