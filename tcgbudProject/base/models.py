4d9b4b58ac68a1d33e889c7179f7c0b7bb1bf1cf
westrix72 <lfeliperebello@live.com>
2023-10-30 22:52:52 -0300

    migratin new models

--- tcgbudProject/base/models.py
+++ tcgbudProject/base/models.py
@@ -9,7 +9,7 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
